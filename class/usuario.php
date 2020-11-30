b1d0f4bc8e6babff0c54eca18949e2a59fd1665d
emerson klimkowski <emersonklim@hotmail.com>
2020-11-30 12:15:27 -0300

    criando selects na class ussuario

--- class/usuario.php
+++ class/usuario.php
@@ -58,6 +58,42 @@ class Usuario{
         }
     }
     
+    public static function getList(){
+        
+        $sql = new Sql();
+        return $sql->select("select * from login order by nome");
+    }
+    
+    public static function search($login){
+        
+        $sql = new Sql();
+        return $sql->select ("select * from login where nome like :search", array(
+            ":search"=>"%" . $login . "%"
+        ));
+    }
+    
+    public function login($login, $password){
+        
+        $sql = new Sql();
+        $results = $sql->select("select * from login where email = :email and senha = :senha", array(
+            ":email" => $login, ":senha"=> $password
+        ));
+        
+        if(count($results) > 0){
+            
+            $row = $results[0];
+            
+            $this->setIdlogin($row['login_id']);
+            $this->setNome($row['nome']);
+            $this->setEmail($row['email']);
+            $this->setSenha($row['senha']);
+            
+        }else{
+            
+            throw new Exception("E-mail e(ou) senha inválidos");
+        }
+    }
+    
     public function __toString() {
         
         return json_encode(array(
--- index.php
+++ index.php
@@ -2,8 +2,23 @@
 
 require_once ("config.php");
 
+//carrega um usuario
+//$usuario = new Usuario();
+//$usuario->loadByid(1);
+
+//carrega todos os usuarios
+//$usuario = Usuario::getList();
+
+//carrega atraves de uma pesquisa
+//$login = "Emer";
+//$usuario = Usuario::search($login);
+//echo json_encode($usuario);
+
+//carrega validando usuario e senha
 $usuario = new Usuario();
-$usuario->loadByid(1);
+$email = "emersonklim@hotmail.com";
+$senha = "123";
+$usuario->login($email, $senha);
 
 echo $usuario;
 
